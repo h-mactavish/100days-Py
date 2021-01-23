0a2141d9175c6db6dc4045aea877ca3db9a6aa53
Himanshu MacTavish <Himanshu.mactavish@gmail.com>
2021-01-23 22:00:49 +0530

    Day 2-Edits

---
+++ 2.py
@@ -0,0 +1,13 @@
+# Day 2 -Primitive Types
+
+print("hello"[1])
+n = 2
+strign = str(n)
+print(type(n))
+
+print(strign)
+print(type(strign))
+
+a = float(123)
+print(70+a)
+print(str(70)+str(a))
---
+++ day-2_exercise1.py
@@ -0,0 +1,5 @@
+# Adding two digits
+
+print("Enter your number: ")
+num = input()
+print(int(num[0])+int(num[1]))
