8cc44a7ec89649597e8e52a654133fb3eb2bd8db
Himanshu MacTavish <Himanshu.mactavish@gmail.com>
2021-01-25 17:32:44 +0530

    Did exercise 1

--- 3.py
+++ 3.py
@@ -4,7 +4,7 @@ print("Welcome to the roller coaster ride\n")
 
 height = int(input("Please Enter your height in cm?"))
 
-if height > 120:
+if height >= 120:
     print("\nYou can ride the rollercoaster")
 else:
     print("\nSorry you need to grow taller before you can ride")
