3c49c5ed23ee5c8362f801b7e8bc1ef4a2b03c41
Himanshu MacTavish <Himanshu.mactavish@gmail.com>
2021-01-25 17:28:54 +0530

    Start of day-3

---
+++ 3.py
@@ -0,0 +1,10 @@
+# Day 3-Control Flow and Logical Operators
+
+print("Welcome to the roller coaster ride\n")
+
+height = int(input("Please Enter your height in cm?"))
+
+if height > 120:
+    print("\nYou can ride the rollercoaster")
+else:
+    print("\nSorry you need to grow taller before you can ride")
