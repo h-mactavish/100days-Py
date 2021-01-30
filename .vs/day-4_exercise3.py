9e75b3a2717b37ff5822fdf34c00f18265fc0543
Himanshu MacTavish <Himanshu.mactavish@gmail.com>
2021-01-30 11:29:25 +0530

    Did exercise 3 day 4

--- .vs/day-4_exercise3.py
+++ .vs/day-4_exercise3.py
@@ -4,12 +4,18 @@ row2 = ["⬜️", "⬜️", "⬜️"]
 row3 = ["⬜️", "⬜️", "⬜️"]
 map = [row1, row2, row3]
 print(f"{row1}\n{row2}\n{row3}")
-position = input("Where do you want to put the treasure? ")
+position = int(input("Where do you want to put the treasure? "))
 # 🚨 Don't change the code above 👆
 
 # Write your code below this row 👇
 
+column = int(position/10)-1
+row = int(position % 10)-1
 
+row_edit = map[row]
+
+row_edit[column] = "X"
+# print(row_edit)
 # Write your code above this row 👆
 
 # 🚨 Don't change the code below 👇
