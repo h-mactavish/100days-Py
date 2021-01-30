852f5bf33480f5e0481eff014d59300ca32fc2ca
Himanshu MacTavish <Himanshu.mactavish@gmail.com>
2021-01-30 12:03:30 +0530

    Fixed day-4 projet

--- .vs/day-4_project.py
+++ .vs/day-4_project.py
@@ -40,23 +40,42 @@ print(choices[user_choice])
 print("CPU choose: \n")
 print(choices[cpu_choice])
 
-if(cpu_choice == user_choice):
+# Hard Code Solution
+# if(cpu_choice == user_choice):
+#     print("Its a draw")
+
+# if(cpu_choice == 0 and user_choice == 1):
+#     print("You Win")
+
+# if(cpu_choice == 0 and user_choice == 2):
+#     print("You Lose")
+
+# if(cpu_choice == 1 and user_choice == 0):
+#     print("You Win")
+
+# if(cpu_choice == 1 and user_choice == 2):
+#     print("You Win")
+
+# if(cpu_choice == 2 and user_choice == 0):
+#     print("You Win")
+
+# if(cpu_choice == 2 and user_choice == 1):
+#     print("You Lose")
+
+if (cpu_choice == 00 and user_choice == 2):
+    print("You Lose")
+
+elif (user_choice == 0 and cpu_choice == 2):
+    print("You Win")
+
+elif (cpu_choice > user_choice):
+    print("You Lose")
+
+elif (user_choice > cpu_choice):
+    print("You Lose")
+
+elif (cpu_choice == user_choice):
     print("Its a draw")
 
-if(cpu_choice == 0 and user_choice == 1):
-    print("You Win")
-
-if(cpu_choice == 0 and user_choice == 2):
-    print("You Lose")
-
-if(cpu_choice == 1 and user_choice == 0):
-    print("You Lose")
-
-if(cpu_choice == 1 and user_choice == 2):
-    print("You Win")
-
-if(cpu_choice == 2 and user_choice == 0):
-    print("You Win")
-
-if(cpu_choice == 2 and user_choice == 1):
-    print("You Lose")
+else:
+    print("Invalid number")
