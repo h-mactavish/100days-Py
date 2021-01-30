3cec425b04385a91199c8be5cee05e10b36dc776
Himanshu MacTavish <Himanshu.mactavish@gmail.com>
2021-01-30 19:28:17 +0530

    Did challenge2 day -7

--- Day-7/Hangman-Challenge1/main.py
+++ Day-7/Hangman-Challenge1/main.py
@@ -11,8 +11,8 @@ chosen_word = random.choice(word_list)
 
 # TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 
-guess = input("Guess a letter")
-guess = guess.lower()
+guess = input("Guess a letter: ").lower()
+
 
 # TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
 
