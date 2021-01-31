3495801541a1f8369152e955821bd51b4d5241e9
Himanshu MacTavish <Himanshu.mactavish@gmail.com>
2021-01-31 09:16:30 +0530

    Did day-8 exercise-1

---
+++ Day-8/8.py
@@ -0,0 +1,12 @@
+# Day 8 "Functions with inputs"
+
+# Review
+
+def greet(something):
+    print(f"Hello {something}")
+    print("Still Chillin inside greet")
+    print("Ofcourse im in greet")
+
+
+name = input("Whats Your Name? ")
+greet(name)
