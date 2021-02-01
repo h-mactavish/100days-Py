57df2b2ed12400e6e6603f5de76d844727948fb1
Himanshu MacTavish <Himanshu.mactavish@gmail.com>
2021-02-01 21:37:30 +0530

    Day-9:Dictionaries and Nested Loops

---
+++ Day-9/9.py
@@ -0,0 +1,5 @@
+# Day -9 :Dictionaries and Nesteed loops
+
+
+programming_dictionary = {"Bug": "An error in a program that prevents the program from running as expected.",
+                          "Function": "A piece of code that you can easily call over and over again."}
